648b6e2719d810ddb0164be429ad69bc6ceddae3
Drk~Codey <109389152+Drk-codey@users.noreply.github.com>
2025-10-25 22:30:09 +0100

    feat: integrate Vercel Analytics and Speed Insight

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,16 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />    
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default MyApp
